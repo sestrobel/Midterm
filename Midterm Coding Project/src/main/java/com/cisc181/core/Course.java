2474022aae7bdb95ba5eb2580745a4a247b4d8a0
Elizabeth <Elizabeth@wifi-roaming-128-4-103-209.host.udel.edu>
2016-10-13 13:49:16 -0400

    initial commit

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Course.java
@@ -0,0 +1,14 @@
+package com.cisc181.core;
+
+import java.util.UUID;
+
+import com.cisc181.eNums.eMajor;
+
+public class Course {
+	UUID CourseID;
+	String CourseName;
+	int GradePoints;
+	eMajor Major;
+	//
+
+}
