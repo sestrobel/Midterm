424cc8030e511fb59cd6d030d22268b87fd29593
Elizabeth <Elizabeth@wifi-roaming-128-4-103-209.host.udel.edu>
2016-10-14 12:49:08 -0400

    made up some kidz

--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -16,23 +16,41 @@ import schoolExceptions.PersonException;
 
 public class Student_Test {
 
+	@SuppressWarnings("deprecation")
 	@BeforeClass
 	public static void setup() throws PersonException {
 		/*
-		 * Add three Course records, add them to an ArrayList of Course
-Add two Semesters, one for Fall, one for Spring.  Add them to an ArrayList of Semester
-Add two Sections for each Course & Semester (total of six Sections).  Add them to an ArrayList of Section
-Create ten Student records, add them to an ArrayList of Student.
+		 * Add three Course records, add them to an ArrayList of Course Add two
+		 * Semesters, one for Fall, one for Spring. Add them to an ArrayList of
+		 * Semester Add two Sections for each Course & Semester (total of six
+		 * Sections). Add them to an ArrayList of Section Create ten Student
+		 * records, add them to an ArrayList of Student.
 		 */
-		ArrayList classes = new ArrayList<Course>();
-		ArrayList semesters = new ArrayList<Semester>();
-		ArrayList sections = new ArrayList<Section>();
+		ArrayList<Course> classes = new ArrayList<Course>();
+		ArrayList<Semester> semesters = new ArrayList<Semester>();
+		ArrayList<Section> sections = new ArrayList<Section>();
 
-
-
-		ArrayList kidz = new ArrayList<Student>();
-		kidz.add(new Student("Jane", "Doe", "Dough", new Date(90,1,1), eMajor.COMPSI,
-				"Outer Space", "(444)-444-4444", "yooo@gmail.com"));
+		ArrayList<Student> kidz = new ArrayList<Student>();
+		kidz.add(new Student("Jane", "Doe", "Dough", new Date(95, 1, 1), eMajor.COMPSI, "Outer Space", "(444)-444-4444",
+				"yo@gmail.com"));
+		kidz.add(new Student("Riquon", "Lobster", "Smith", new Date(95, 3, 3), eMajor.BUSINESS,
+				"123 Congress Drive, the Mall, DC ", "(111)-111-1111", "yoo@gmail.com"));
+		kidz.add(new Student("Sarah", "Elizabeth", "Lobster", new Date(95, 1, 1), eMajor.COMPSI, "Mercury",
+				"(444)-444-4144", "yooo@gmail.com"));
+		kidz.add(new Student("The", "Notorious", "BIG", new Date(95, 1, 1), eMajor.BUSINESS, "Venus", "(144)-444-4444",
+				"yoooo@gmail.com"));
+		kidz.add(new Student("What", "Ami", "Doing", new Date(95, 1, 1), eMajor.NURSING, "Earth", "(444)-414-4444",
+				"yooooo@gmail.com"));
+		kidz.add(new Student("Who", "Am", "I", new Date(95, 1, 1), eMajor.CHEM, "Mars", "(444)-454-4444",
+				"yoooooo@gmail.com"));
+		kidz.add(new Student("What", "Ismy", "Quest", new Date(95, 1, 1), eMajor.COMPSI, "Jupiter", "(444)-445-4444",
+				"yooooooo@gmail.com"));
+		kidz.add(new Student("John", "Stuart", "Lobster", new Date(95, 1, 1), eMajor.PHYSICS, "Saturn", "(444)-444-4445",
+				"yoooooooo@gmail.com"));
+		kidz.add(new Student("Em", "And", "Em", new Date(95, 1, 1), eMajor.NURSING, "Uranus", "(454)-444-4444",
+				"yoooooooooo@gmail.com"));
+		kidz.add(new Student("Marshall", "Philip", "Mathers", new Date(95, 1, 1), eMajor.CHEM, "Neptune", "(444)-444-4544",
+				"yooooooooooo@gmail.com"));
 	}
 
 	@Test
